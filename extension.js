14f82a62041e02d9f53de743d9bd4ed18f2ef03e
Florian Müllner <fmuellner@gnome.org>
2014-10-31 19:37:59 +0000

    Animate the logo

--- extension.js
+++ extension.js
@@ -3,8 +3,10 @@ const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const St = imports.gi.St;
 
+const Background = imports.ui.background;
 const Layout = imports.ui.layout;
 const Main = imports.ui.main;
+const Tweener = imports.ui.tweener;
 
 const LOGO_URI = 'file:///usr/share/icons/hicolor/scalable/apps/start-here.svg';
 
@@ -14,7 +16,8 @@ const BackgroundLogo = new Lang.Class({
     _init: function(bgManager) {
         this._bgManager = bgManager;
 
-        this.actor = new St.Widget({ layout_manager: new Clutter.BinLayout() });
+        this.actor = new St.Widget({ layout_manager: new Clutter.BinLayout(),
+                                     opacity: 0 });
         bgManager._container.add_actor(this.actor);
 
         let monitorIndex = bgManager._monitorIndex;
@@ -40,12 +43,19 @@ const BackgroundLogo = new Lang.Class({
         let defaultUri = background._settings.get_default_value('picture-uri');
         let file = Gio.File.new_for_commandline_arg(defaultUri.deep_unpack());
 
+        let visible;
         if (background._file) // > 3.14
-            this.actor.visible = background._file.equal(file);
+            visible = background._file.equal(file);
         else if (background._filename) // <= 3.14
-            this.actor.visible = background._filename == file.get_path();
+            visible = background._filename == file.get_path();
         else // background == NONE
-            this.actor.visible = false;
+            visible = false;
+
+        Tweener.addTween(this.actor,
+                         { opacity: visible ? 255 : 0,
+                           time: Background.FADE_ANIMATION_TIME,
+                           transition: 'easeOutQuad'
+                         });
     },
 
     _backgroundDestroyed: function() {
